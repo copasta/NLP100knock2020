60ce56c552c06f0dcd964421f19196b558de23c2
copasta <yonekawakazuhito@NekoBook.local>
2020-04-08 02:49:14 +0900

    Update prob10.py

--- chap02/prob10.py
+++ chap02/prob10.py
@@ -1,3 +1,10 @@
-with open('./data/popular-names.txt') as f:
+import pandas as pd
+
+"""
+with open("./data/popular-names.txt") as f:
     lines = f.readlines()
     print(len(lines))
+"""
+
+popular_name_df = pd.read_csv("./data/popular-names.txt", header=None, sep='\t')
+print(popular_name_df.shape[0])
