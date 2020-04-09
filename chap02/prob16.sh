4e6232a6192c7ff21f88c31617f8aa70191313cc
copasta <yonekawakazuhito@NekoBook.local>
2020-04-10 03:01:02 +0900

    Update shell prob16

--- chap02/prob16.sh
+++ chap02/prob16.sh
@@ -1,2 +1,4 @@
 #split -l 500 ./data/popular-names.txt -d --additional-suffix=.txt ./data/prob16_
-split -l 500 ./data/popular-names.txt ./data/prob16_
+n=`cat ./data/popular-names.txt | wc -l`
+len=`expr $n / 5`
+split -l $len ./data/popular-names.txt ./data/prob16_
