0e4658622cd9f0cbf40244df0226a8c571c0ec84
copasta <47898905+copasta@users.noreply.github.com>
2020-04-15 14:18:15 +0900

    Update prob54.py

--- chap06/prob54.py
+++ chap06/prob54.py
@@ -6,12 +6,12 @@ from sklearn.metrics import accuracy_score
 X_train = pd.read_table('./data/train.feature.txt')
 y_train = pd.read_table('./data/train.txt')
 
-X_valid = pd.read_table('./data/valid.feature.txt')
-y_valid = pd.read_table('./data/valid.txt')
+X_test = pd.read_table('./data/test.feature.txt')
+y_test = pd.read_table('./data/test.txt')
 
 model = joblib.load("./data/model_ch06.joblib")
 train_pred = model.predict(X_train.values)
-valid_pred = model.predict(X_valid.values)
+test_pred = model.predict(X_test.values)
 
 print("train acc: {:.5f}".format(accuracy_score(y_train["CATEGORY"], train_pred)))
-print("valid acc: {:.5f}".format(accuracy_score(y_valid["CATEGORY"], valid_pred)))
+print("test acc: {:.5f}".format(accuracy_score(y_test["CATEGORY"], test_pred)))
